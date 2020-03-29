fe509cac13b905148525f0a9c00761fb1c310695
Seth Fenske <fenskesd@msoe.edu>
2020-03-29 13:03:50 -0500

    Player now falls. Also can detect invalid waypoint entries

--- Scripts/Waypoint.gd
+++ Scripts/Waypoint.gd
@@ -1,11 +1,25 @@
 extends Area
 
+export(Array, String) var Edges
+var children = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	connect("body_entered", self, "_on_body_entered")
+	var waypoint_node = get_tree().get_root().get_node("Spatial/Waypoints")
+	for node_name in Edges:
+		children.append(waypoint_node.get_node(node_name))
+	print("Node " + name + ": has " + String(children.size()) + " children.")
 
-
-
-func _on_Area_body_entered(body):
+func _on_body_entered(body):
 	if(body.get_name()=="ski-body"):
-		body.printMessage()
+		body.process_next_node(self)
+
+func has_valid_child(waypoint):
+	for child in children:
+		if(waypoint.name == child.name):
+			return true
+	# Happens when we drop player back on the node to get back in bounds
+	if waypoint.name == name:
+		return true
+	return false
--- Scripts/ski_body.gd
+++ Scripts/ski_body.gd
@@ -1,9 +1,28 @@
 extends KinematicBody
 
+onready var current_node
+var out_of_bounds = false
+var velocity = Vector3.ZERO
+var Terminal_Velocity = 200
+
+func _ready():
+	current_node = get_tree().get_root().get_node("Spatial/Waypoints/start")
+	print(current_node.name)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var targetDirection = Vector3(0, 0, 1) * delta
-	transform.origin += targetDirection
+	if out_of_bounds:
+		print("Out of Bounds")
+	velocity = velocity + (Vector3.DOWN * 9.81)
+	if(velocity.length() >= Terminal_Velocity):
+		velocity = velocity.normalized() * Terminal_Velocity
+	move_and_slide(velocity * delta)
 
 func printMessage():
 	print("This is a message")
+
+func process_next_node(next_node):
+	if current_node.has_valid_child(next_node):
+		current_node = next_node
+	else:
+		out_of_bounds = true
