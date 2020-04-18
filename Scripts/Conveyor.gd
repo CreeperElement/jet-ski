846a0c9dc23631317cca276b67f82062e331458e
Seth Fenske <fenskesd@msoe.edu>
2020-04-18 16:19:09 -0500

    Fixed error where box files off conveyor after leaving it

--- Scripts/Conveyor.gd
+++ Scripts/Conveyor.gd
@@ -5,12 +5,22 @@ var bodies = []
 func _ready():
 	print($PushArea)
 	$PushArea.connect("body_entered", self, "_on_push_entered")
+	$PushArea.connect("body_exited", self, "_on_push_exited")
 
 func _on_push_entered(body):
 	if body.get_name() != "pushable":
 		return
 	bodies.append(body)
 
+func _on_push_exited(body):
+	body.velocity = Vector3(body.velocity.x, 0, body.velocity.z)
+
 func _physics_process(delta):
 	for body in bodies:
-		body.RigidBody.set_linear_velocity(self.get_global_transform.basis.z)
+		set_velocity(body)
+
+func set_velocity(body):
+	var new_velocity = body.velocity
+	var localBasis = self.get_global_transform().basis.z
+	new_velocity = Vector3(localBasis.x, -1, localBasis.z)
+	body.velocity = new_velocity
