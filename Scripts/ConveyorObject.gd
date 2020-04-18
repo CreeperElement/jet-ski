aec19b00b2ce1987d521df5105960ec0278f35e2
Seth Fenske <fenskesd@msoe.edu>
2020-04-18 15:36:25 -0500

    Box now falls

--- Scripts/ConveyorObject.gd
+++ Scripts/ConveyorObject.gd
@@ -1,4 +1,18 @@
-extends RigidBody
+extends KinematicBody
+
+var Gravity = Vector3(0, -9, 0)
+var MAX_FALL = 10
+
+onready var velocity: Vector3
+
+func _ready():
+	velocity = Vector3(0, 0, 0)
 
 func get_name():
 	return "pushable"
+
+func _physics_process(delta):
+	velocity += Gravity*delta
+	if velocity.y >= MAX_FALL:
+		velocity.y = MAX_FALL
+	move_and_slide(velocity)
